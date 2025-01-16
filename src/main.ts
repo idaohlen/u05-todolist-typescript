08318132184b4a745fd147b3e0ff7760ee0f4ab6
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2025-01-16 10:19:34 +0100

    login page styling

--- src/main.ts
+++ src/main.ts
@@ -28,8 +28,14 @@ function renderLoginPage() : void {
       <iconify-icon icon="fa6-solid:pen-nib" class="app-header__icon"></iconify-icon>
     </header>
     <div class="login-form">
-      <input type="email" placeholder="email" id="emailInput">
-      <input type="password" placeholder="password" id="passwordInput">
+      <div class="login-form__row">
+        <iconify-icon icon="solar:letter-outline" class="login-form__icon"></iconify-icon>
+        <input type="email" placeholder="email" id="emailInput">
+      </div>
+      <div class="login-form__row">
+        <iconify-icon icon="solar:lock-password-outline" class="login-form__icon"></iconify-icon>
+        <input type="password" placeholder="password" id="passwordInput">
+      </div>
       <div class="login-form__buttons">
         <button id="registerUserBtn" class="btn btn--outline">Register</button>
         <button id="loginBtn" class="btn">Login</button>
